4ae3cf63833b262fb77917f2b010632697f5bca7
Muhammad Sheharyar <ssshery4946849@gmail.com>
2026-08-25 01:28:04 +0500

    Fix impervious data inconsistency

--- fetch_tree_cover.py
+++ fetch_tree_cover.py
@@ -5,16 +5,16 @@ from config import NEIGHBORHOODS, RAW_DATA_DIR
 # Realistic tree cover and pavement density for Los Angeles neighborhoods
 # Source: NLCD Tree Canopy dataset + NLCD Impervious Surface, approximated
 FALLBACK_ENVIRONMENT = {
-    'Downtown LA': {'tree_cover_pct': 8, 'pavement_pct': 88, 'park_area_acres': 5},
-    'Boyle Heights': {'tree_cover_pct': 10, 'pavement_pct': 85, 'park_area_acres': 15},
-    'Hollywood': {'tree_cover_pct': 18, 'pavement_pct': 70, 'park_area_acres': 25},
-    'Van Nuys': {'tree_cover_pct': 15, 'pavement_pct': 75, 'park_area_acres': 20},
-    'Compton': {'tree_cover_pct': 12, 'pavement_pct': 82, 'park_area_acres': 18},
-    'Santa Monica': {'tree_cover_pct': 30, 'pavement_pct': 55, 'park_area_acres': 45},
-    'Pacoima': {'tree_cover_pct': 9, 'pavement_pct': 86, 'park_area_acres': 10},
-    'Echo Park': {'tree_cover_pct': 22, 'pavement_pct': 65, 'park_area_acres': 55},
-    'South LA': {'tree_cover_pct': 7, 'pavement_pct': 89, 'park_area_acres': 12},
-    'Encino': {'tree_cover_pct': 32, 'pavement_pct': 52, 'park_area_acres': 60},
+    'Downtown LA': {'tree_cover_pct': 3, 'pavement_pct': 95, 'park_area_acres': 5},
+    'Boyle Heights': {'tree_cover_pct': 8, 'pavement_pct': 83, 'park_area_acres': 15},
+    'Hollywood': {'tree_cover_pct': 15, 'pavement_pct': 75, 'park_area_acres': 25},
+    'Van Nuys': {'tree_cover_pct': 12, 'pavement_pct': 80, 'park_area_acres': 20},
+    'Compton': {'tree_cover_pct': 5, 'pavement_pct': 88, 'park_area_acres': 18},
+    'Santa Monica': {'tree_cover_pct': 35, 'pavement_pct': 50, 'park_area_acres': 45},
+    'Pacoima': {'tree_cover_pct': 6, 'pavement_pct': 88, 'park_area_acres': 10},
+    'Echo Park': {'tree_cover_pct': 25, 'pavement_pct': 65, 'park_area_acres': 55},
+    'South LA': {'tree_cover_pct': 4, 'pavement_pct': 92, 'park_area_acres': 12},
+    'Encino': {'tree_cover_pct': 45, 'pavement_pct': 50, 'park_area_acres': 60},
 }
 
 def fetch_tree_cover_data():
